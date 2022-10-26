f6a4a415af61f616f5a49ae508d228e518a1e197
LindaTrana <lindatrana06@gmail.com>
2022-10-26 09:13:34 -0600

    actualizando

--- PROYECTO_PRACTICAS/CAPA_NEGOCIO/Dañados y Devoluciones/DetalleDanados.cs
+++ PROYECTO_PRACTICAS/CAPA_NEGOCIO/Dañados y Devoluciones/DetalleDanados.cs
@@ -9,7 +9,6 @@ namespace CAPA_NEGOCIO
 {
     public class DetalleDanados
     {
-<<<<<<< HEAD
        
             public string TableName = "DETALLEDANADOS";
             public int IdDetalleD { get; set; }
@@ -21,24 +20,11 @@ namespace CAPA_NEGOCIO
             public int IdTalla { get; set; }
             public int IdMarca { get; set; }
             public int IdBodega { get; set; }
-=======
-
-        public string TableName = "DETALLEDANADOS";
-        public int IdDetalleD { get; set; }
-        public int IdDanados { get; set; }
-        public int IdInventario { get; set; }
-        public int IdProducto { get; set; }
-        public string CodigoProducto { get; set; }
-        public int Cantidad { get; set; }
-        public int IdTalla { get; set; }
-        public int IdMarca { get; set; }
-        public int IdBodega { get; set; }
->>>>>>> 41c719d25a772fe85e3601dee3320ea0cfe88d19
 
 
 
         public Object TraerDetalleD(DetalleDanados Inst)
-<<<<<<< HEAD
+
             {
                 try
                 {
@@ -52,18 +38,4 @@ namespace CAPA_NEGOCIO
             }
     }
 }
-=======
-        {
-            try
-            {
-                return SqlADOConexion.SQLM.InsertObject(TableName, Inst);
-            }
-            catch (Exception)
-            {
 
-                throw;
-            }
-        }
-    }
-}
->>>>>>> 41c719d25a772fe85e3601dee3320ea0cfe88d19
